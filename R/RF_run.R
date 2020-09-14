0531ec93a6bf5096dc8645a306068f9b15cb0ea9
jdaeth274 <joshdaeth@yahoo.co.uk>
2020-09-14 15:41:54 +0100

    updating RF_run script to work in bash pipeline

--- R/RF_run.R
+++ R/RF_run.R
@@ -608,18 +608,23 @@ results_testing_plotter <- function(results_df, column_to_use,y_lab, pdf_out_fil
 ## Right, now we'll run on the pmen known data to test fitting to the data ####
 ###############################################################################
 
+input_args <- commandArgs(trailingOnly = TRUE)
+test_set <- input_args[1]
+training_set <- input_args[2]
+number_of_categories <- input_args[3]
+out_csv_name <- input_args[4]
+
 ## load up the AA csv from the bash script here 
-whole_aa_data_sample <- read.csv(file = "~/Dropbox/phd/cdc_mic_predictions/all_isolates_with_three_pbp_profiles_db_updated.csv",
+whole_aa_data_sample <- read.csv(file = test_set,
                                stringsAsFactors = TRUE)
 
 ## Ensure T & F treated as characters not boolean
-whole_aa_data_pmen <- true_changer(whole_aa_data_pmen)
+whole_aa_data <- true_changer(whole_aa_data_sample)
 
-## Remove any missing values 
-whole_aa_data <- na.omit(whole_aa_data_pmen)
+## Remove any isolates with missing values 
+whole_aa_data <- whole_aa_data[complete.cases(whole_aa_data),]
 
-
-cdc_data <- read.csv("./data/cdc_seqs_df.csv")
+cdc_data <- read.csv(training_set)
 
 cdc_data <- true_changer(cdc_data)
 
@@ -634,15 +639,18 @@ cdc_data_no_isolate$mic <- log(cdc_data_no_isolate$mic, base = 2)
 
 ## Test on CDC data only 
 cdc_known_mic <- rf_model_fitter(cdc_data_no_isolate, ranger_use = TRUE,
-                                 categories = 3)
+                                 categories = number_of_categories)
 
-## Predict Unknown data noe
+## Predict Unknown data now
 cdc_preds <- rf_model_fitter(cdc_data_no_isolate,test_data = whole_aa_data,
-                             ranger_use = TRUE, categories = 3)
+                             ranger_use = TRUE, categories = number_of_categories)
 
 ### Predicted categories for penicillin below
 
 predicted_categories <- cdc_preds$valid_preds
 
+out_df <- cbind.data.frame(whole_aa_data$id, predicted_categories)
+colnames(out_df) <- c("id","penicillin_cat")
 
-
+write.csv(out_df,
+          file = out_csv_name)
